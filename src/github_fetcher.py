05107deeb00b1a0306bbddecd09253ca40efb9c2
Alvin <alvin.pastore@gmail.com>
2020-02-20 14:35:51 +0000

    dump json or prettyprint (using dummy issues_info), also explicitly target open issues

--- src/github_fetcher.py
+++ src/github_fetcher.py
@@ -1,4 +1,6 @@
 import requests
+import json
+import pprint
 
 
 def fetch(repo_name, repo_owner, n=5, auth=None, output_filepath=None):
@@ -13,7 +15,7 @@ def fetch(repo_name, repo_owner, n=5, auth=None, output_filepath=None):
             output_filepath: folder where to serialize the retrieved issues
     """
 
-    url = 'https://api.github.com/repos/' + repo_owner + '/' + repo_name + '/issues'
+    url = 'https://api.github.com/repos/' + repo_owner + '/' + repo_name + '/issues?state=open'
 
     if not auth:
         with open('../misc/git_token', 'r') as api_token_file:
@@ -23,16 +25,43 @@ def fetch(repo_name, repo_owner, n=5, auth=None, output_filepath=None):
     print('Accessing ' + url)
 
     r = requests.get(url, headers=headers)
+    json_info = r.json()
 
-    print(r.status_code)
-    print(r.headers['content-type'])
-    print(r.encoding)
-    print(r.text)
-    print(r.json())
+    # TODO iterate up to n issues and populate issues_info
+
+    # dummy
+    issues_info = {
+        "author_username": 'STRING',
+        "reactions": {
+            "total_count": 'INT',
+            "+1": 'INT',
+            "-1": 'INT',
+            "laugh": 'INT',
+            "confused": 'INT',
+            "heart": 'INT',
+            "hooray": 'INT'
+        },
+        "repository_url": 'STRING',
+        "issue_number": 'INT',
+        "title": 'STRING',
+        "body": 'STRING',
+        "labels": 'LIST[STRING]  Just label names',
+        "number_of_comments": 'INT',
+        "created_at": 'TIMESTAMP',
+        }
+
+    if output_filepath:
+        with open(output_filepath + '/' + repo_owner+'_'+repo+'.json') as dump_file:
+            json.dump(issues_info, dump_file)
+        pass
+    else:
+        pprint.pprint(issues_info, width=60)
 
 
 if __name__ == '__main__':
     owner = 'rust-lang'
     repo = 'rust'
-    fetch(repo, owner)
+    num_issues = 400
+
+    fetch(repo, owner, n=num_issues, output_filepath='data')
 
